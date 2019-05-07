5e7948d31f677d353084ceded362faf2af12fecb
mragon <91610202@qq.com>
2019-05-07 14:20:30 +0800

    [feat]购物车页面联调

--- shopping-pc/src/views/Cart.vue
+++ shopping-pc/src/views/Cart.vue
@@ -1,56 +1,90 @@
 <template>
   <div class="cart">
-    <el-table :data="tableData" style="width: 100%">
-      <el-table-column prop="date" label="商品" ></el-table-column>
-      <el-table-column prop="name" label="数量" width="180"></el-table-column>
-      <el-table-column prop="address" label="总价" width="180"></el-table-column>
+    <el-table :data="cartList" style="width: 100%">
+      <el-table-column prop="detail.name" label="商品" align="center"></el-table-column>
+      <el-table-column prop="productNum" label="数量" align="center"></el-table-column>
+      <el-table-column prop="sumPrice" label="总价" align="center"></el-table-column>
     </el-table>
+    <div class="cart-bar">
+      <div class="total-price">合计：{{totalPrice}}</div>
+      <div class="cart-pay">
+        <el-button type="success">立即结算</el-button>
+      </div>
+    </div>
   </div>
+  
 </template>
 <script>
 export default {
   data() {
     return {
-      tableData: [
-        {
-          date: "2016-05-02",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1518 弄"
-        },
-        {
-          date: "2016-05-04",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1517 弄"
-        },
-        {
-          date: "2016-05-01",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1519 弄"
-        },
-        {
-          date: "2016-05-03",
-          name: "王小虎",
-          address: "上海市普陀区金沙江路 1516 弄"
-        }
-      ]
+      userId: -1, // 当前登录用户的userId  为-1时表示用户未登录
+      cartList: [] // 购物车列表
     };
   },
   mounted() {
-    console.log('挂载Cart组件')
+    console.log("挂载Cart组件");
 
+    // 获取用户id
+    this.getUserId();
 
+    // 获取购物车列表
+    this.getCartList()
   },
   methods: {
+    /**
+     * 获取用户id
+     */
+    getUserId() {
+      // 当前用户的登录
+      let isLogin = this.$store.state.loginRole !== 0;
+      let userId = this.$store.state.userId;
+      console.log("当前用户是否登录：", isLogin);
+      console.log("当前登录用户的userId：", userId);
+      if (isLogin) {
+        this.userId = userId;
+      } else {
+        this.$router.push({
+          name: "login"
+        });
+        this.$message({
+          message: "用户未登录，请登录后重试",
+          type: "warning"
+        });
+      }
+    },
     /**
      * 获取购物车列表
      */
     getCartList() {
-      
+      console.log("获取购物车列表");
+      this.axios
+        .get(`http://127.0.0.1:7001/api/usercart/${this.userId}`)
+        .then(res => {
+          console.log("获取购物车列表返回参数", res);
+          this.cartList = res.data.data
+        });
     }
-
+  },
+  computed: {
+    totalPrice() {
+      let sum = 0
+      this.cartList.forEach(cartItem => {
+        sum += cartItem.sumPrice
+      })
+      return sum
+    }
   }
 };
 </script>
 
 <style scoped>
+.cart-bar {
+  float: right;
+  margin-top: 100px;
+}
+.total-price {
+  height: 50px;
+  line-height: 50px;
+}
 </style>
